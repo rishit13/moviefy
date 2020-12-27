d360a5c7c5dbd8b6c837d2c28bb4ba142e420b01
Your Name <you@example.com>
2020-12-27 12:56:35 +0530

    Deployed versions

--- app_1.py
+++ app_1.py
@@ -4,14 +4,10 @@ Created on Sat Dec 26 18:26:48 2020
 
 @author: Admin
 """
-from __future__ import division, print_function
+
 import os
-import sys
-import glob
 from flask import Flask, render_template, request, redirect,url_for
 from source_1 import capture_1
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
 app_1 = Flask(__name__)
 
 
